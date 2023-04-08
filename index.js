92dd630109755299f7a3662fef00c5d1dc267db8
HOYEON KIM <hoyeoness9837@gmail.com>
2023-04-08 14:13:19 +0900

    version updated: index.html - clearBtn is added - stars will show up based on priority - removed title - repositioned error message tag
    index.js
    - removed previous comments
    - remove console.log()
    - error meesage disappears after 3 sec
    - show how long it took to complete
    - using static method to parse task item with stars-rating and
    - using bind() to bind methods in the same class and use it for rendering pages.
    - instead of using backtick '``' to create elements for html, use createElement(), className, classList.add(), textContent, and appendChild().
    
    index.css
    - style updated.

--- index.js
+++ index.js
@@ -1,116 +1,198 @@
-// const input = document.getElementById('todoInput');
-// const listNode = document.getElementById('todo-list');
-// const errorMessage = document.getElementById('error-message');
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const task = input.value;
-
-//   // 입력된 값이 없다면 에러메세지 렌더.
-//   if (!task) {
-//     errorMessage.textContent = '할 일을 입력해주세요.';
-//     return;
-//   }
-//   const listElem = document.createElement('li');
-//   listElem.innerText = task;
-//   listNode.appendChild(listElem);
-
-//   // 입력창 리셋
-//   input.value = '';
-// }
-
-// document.querySelector('form').addEventListener('submit', handleSubmit);
-
-class Todo {
-  constructor(title, description, priority) {
-    this.title = title;
-    this.description = description;
-    this.priority = priority;
+// 각 투두객체의 기본 스트럭쳐.
+class TodoItem {
+  constructor(text, priority) {
+    this.id = ParseItem.generateUniqueId();
+    this.text = text;
     this.completed = false;
+    this.stars = ParseItem.convertToStars(priority);
     this.createdAt = new Date();
     this.completedAt = null;
   }
 }
 
-class TodoList {
-  constructor() {
-    this.list = [];
+class ParseItem {
+  // 각 투두에 붙여질 유니크아이디 생성함수.
+  static generateUniqueId() {
+    return 'xxxx-yxxx'.replace(/[xy]/g, function (c) {
+      const r = (Math.random() * 16) | 0;
+      const v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
   }
 
-  add(todo) {
-    let i = 0;
-    while (i < this.list.length && this.list[i].priority >= todo.priority) {
-      i++;
+  //우선순위의 숫자 크기에 따라 별 모영의 갯수로 중요도를 표시.
+  static convertToStars(num) {
+    let stars = '';
+    for (let i = 0; i < num; i++) {
+      stars += '⭐';
     }
-    this.list.splice(i, 0, todo);
+    return stars;
   }
 
-  remove(index) {
-    this.list.splice(index, 1);
+  static getTimeDifference(start, end) {
+    // 시작 시간과 끝 시간의 차이를 밀리초 단위로 계산
+    const diff = Math.abs(end.getTime() - start.getTime());
+
+    // 차이를 일, 시간, 분, 초 단위로 변환
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    // 결과를 텍스트로 반환
+    let result = '';
+    if (hours > 0) {
+      result += `${hours}hrs `;
+    }
+    if (minutes > 0) {
+      result += `${minutes}min `;
+    }
+    if (seconds > 0) {
+      result += `${seconds}sec`;
+    }
+    return result;
+  }
+}
+
+// client가 입력한 텍스트를 이용하여 로컬스토리지에 CRUD 하고, 요소들을 생성해 렌더링해줌.
+class TodoList {
+  // 투두의 디폴트 상태
+  constructor() {
+    this.todoItems = [];
+    this.init(); // 생성자에서 메소드를 직접 불러 new TodoList()로 새로운 객체를 만들때 마다 init()이 먼저 작동함.
   }
 
-  toggleCompleted(index) {
-    this.list[index].completed = !this.list[index].completed;
-    this.list[index].completedAt = new Date();
+  init() {
+    // 과거 작성한 기록이 있는지 로드.
+    this.loadFromStorage();
+
+    // 이벤트 핸들러를 폼태그와 버튼에 바인딩하여 DOM 리스닝 하도록 함.
+    const form = document.querySelector('form');
+    form.addEventListener('submit', this.addTodo.bind(this));
+
+    const clearBtn = document.getElementById('clearBtn');
+    clearBtn.addEventListener('click', this.clearAll.bind(this));
+  }
+
+  loadFromStorage() {
+    const todoItemsString = localStorage.getItem('todoItems');
+    if (todoItemsString) {
+      const copytext = JSON.parse(todoItemsString);
+      this.todoItems = copytext;
+      this.renderTodoList();
+    }
+  }
+
+  updateToStorage() {
+    localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
+  }
+
+  addTodo(event) {
+    event.preventDefault();
+    const input = document.getElementById('todoInput');
+    const errorMessage = document.getElementById('error-message');
+    const text = input.value.trim();
+    const priority = document.querySelector('#priority-select');
+    if (text) {
+      const todoItem = new TodoItem(text, Number(priority.value));
+      this.todoItems.push(todoItem);
+      this.renderTodoList();
+      this.updateToStorage();
+      input.value = '';
+    } else {
+      //투두 인풋이 비어있으면 errorMessage 반환
+      errorMessage.textContent = 'PLEASE ENTER TASKS.';
+      setTimeout(() => {
+        errorMessage.textContent = '';
+      }, 3 * 1000);
+    }
+  }
+
+  deleteTodo(id) {
+    const index = this.todoItems.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      this.todoItems.splice(index, 1);
+      this.renderTodoList();
+      this.updateToStorage();
+    }
+  }
+
+  toggleCompleted(id) {
+    const index = this.todoItems.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      this.todoItems[index].completed
+        ? (this.todoItems[index].completedAt = new Date())
+        : (this.todoItems[index].completedAt = null);
+      this.renderTodoList();
+      this.updateToStorage();
+    }
+  }
+
+  //모든 투두 아이템 삭제.
+  clearAll() {
+    this.todoItems = [];
+    this.renderTodoList();
+    this.updateToStorage();
+  }
+
+  renderTodoList() {
+    const todoList = document.getElementById('todoList');
+    todoList.innerHTML = '';
+
+    // 렌더링 되기 이전 높은 우선순위가 맨 위로 올라오도록 정렬.
+    const sortedItems = this.todoItems.sort(
+      (a, b) => b.stars.length - a.stars.length
+    );
+
+    // 각 투두리스트에 들어가는 요소 생성.
+    for (const item of sortedItems) {
+      const todoItemEl = document.createElement('div');
+      todoItemEl.className = 'todoItem';
+
+      const priorityEl = document.createElement('span');
+      priorityEl.classList.add('star');
+      priorityEl.textContent = item.stars;
+      todoItemEl.appendChild(priorityEl);
+
+      const checkboxEl = document.createElement('input');
+      checkboxEl.type = 'checkbox';
+      checkboxEl.checked = item.completed;
+      checkboxEl.addEventListener(
+        'change',
+        this.toggleCompleted.bind(this, item.id)
+      );
+      todoItemEl.appendChild(checkboxEl);
+
+      const textEl = document.createElement('div');
+      textEl.className = 'text';
+      textEl.textContent = item.text;
+      if (item.completed) {
+        textEl.classList.add('completed');
+      }
+      todoItemEl.appendChild(textEl);
+
+      const timestampEl = document.createElement('small');
+      timestampEl.classList.add('timeStamp');
+      item.completed
+        ? (timestampEl.textContent = `Took ${ParseItem.getTimeDifference(
+            item.createdAt,
+            item.completedAt
+          )} `)
+        : (timestampEl.textContent = `Created At ${item.createdAt.toLocaleString()}`);
+      todoItemEl.appendChild(timestampEl);
+
+      const deleteBtnEl = document.createElement('button');
+      deleteBtnEl.id = 'deleteBtn';
+      deleteBtnEl.textContent = 'Delete';
+      deleteBtnEl.addEventListener(
+        'click',
+        this.deleteTodo.bind(this, item.id)
+      );
+      todoItemEl.appendChild(deleteBtnEl);
+
+      todoList.appendChild(todoItemEl);
+    }
   }
 }
 
 const todoList = new TodoList();
-const form = document.querySelector('form');
-const errorMessage = document.getElementById('error-message');
-const [titleInput, descriptionInput, prioritySelect, list] =
-  document.querySelectorAll(
-    '#title-input, #description-input, #priority-select, #todo-list'
-  );
-console.log(errorMessage);
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const title = titleInput.value.trim();
-  const description = descriptionInput.value.trim();
-  const priority = Number(prioritySelect.value);
-
-  if (!title || !description) {
-    errorMessage.textContent = 'Please enter a title and description.';
-    return;
-  }
-  todoList.add(new Todo(title, description, priority));
-  render();
-  form.reset();
-  errorMessage.textContent = '';
-});
-
-function render() {
-  list.innerHTML = todoList.list
-    .map(
-      (todo, index) => `
-        <li${todo.completed ? ' class="completed"' : ''}>
-            <div>
-                <h3>${todo.title}</h3>
-                <p>${todo.description}</p>
-            </div>
-                <small>${
-                  todo.completed
-                    ? `Completed at: ${todo.completedAt.toLocaleString()}`
-                    : `Created at: ${todo.createdAt.toLocaleString()}`
-                }</small>
-                <br/>
-                <button onclick="toggleCompleted(${index})">${
-        todo.completed ? 'Undo' : 'Done'
-      }</button>
-                <button onclick="remove(${index})">Remove</button>
-        </li>
-      `
-    )
-    .join('');
-}
-
-function remove(index) {
-  todoList.remove(index);
-  render();
-}
-
-function toggleCompleted(index) {
-  todoList.toggleCompleted(index);
-  render();
-}
